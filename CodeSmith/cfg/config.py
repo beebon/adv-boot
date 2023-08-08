78d79495f97d982e9f9207de70038ed1bd29d7d5
bee <beebon@users.noreply.github.com>
2023-08-08 17:08:32 +0800

    Update config.py

--- CodeSmith/cfg/config.py
+++ CodeSmith/cfg/config.py
@@ -6,6 +6,7 @@ config = {
     ,'version':'v1.0'
     ,'package':'com.mycompany.myapp' #命名空间
     ,'file_type' : 'java' #默认文件类型（当不指定生成规则的文件类型时默认使用）
+    ,'use_table_name' : True # 使用表名做为文件名
     ,'temp_dir':'temp/adv' #模板目录，若需自定义模板，可复制该目录模板自行修改
     ,'temp_arr': #模板生成规则：包名:后缀:文件类型:前缀:扩展参数
         ['entity:','controller:Controller','service:Service::I','serviceImpl:ServiceImpl','mapper:Mapper','mapperXml:Mapper:xml','vueList:List:vue','vueModal:Modal:vue']
