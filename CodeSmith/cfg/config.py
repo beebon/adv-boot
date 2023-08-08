33f2318a446221d002e13f7ddde9cc19fb3e03c1
bee <beebon@users.noreply.github.com>
2023-08-08 15:34:01 +0800

    Update config.py注解

--- CodeSmith/cfg/config.py
+++ CodeSmith/cfg/config.py
@@ -9,6 +9,6 @@ config = {
     ,'temp_dir':'temp/adv' #模板目录，若需自定义模板，可复制该目录模板自行修改
     ,'temp_arr': #模板生成规则：包名:后缀:文件类型:前缀:扩展参数
         ['entity:','controller:Controller','service:Service::I','serviceImpl:ServiceImpl','mapper:Mapper','mapperXml:Mapper:xml','vueList:List:vue','vueModal:Modal:vue']
-    #,'models':['Notify']
+    #,'models':['Notify'] #可以仅指定需要生成的模块,不设置则默认数据库所有表都生成
     ,'output' : 'output/example'
 }
